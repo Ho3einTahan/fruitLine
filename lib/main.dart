359e14232cd38b9d6aebcb09614359843ca84832
MaxBased <hoseintahan84@gmail.com>
2024-04-26 01:28:44 +0330

    Update   main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_map/flutter_map.dart';
-import 'package:latlong2/latlong.dart';
-import 'package:url_launcher/url_launcher.dart';
+import 'package:fruitline/core/class/model/cart.dart';
+import 'package:fruitline/core/class/model/map.dart';
+import 'package:fruitline/view/screen/map-screen.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/adapters.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  //
+  Hive.registerAdapter(MapAdapter());
+  Hive.registerAdapter(CartAdapter());
+  //
+  await Hive.openBox<Map>('map');
+  await Hive.openBox<Cart>('cart');
+
   runApp(MyApp());
 }
 
@@ -13,88 +24,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: MapScreen(),
-    );
-  }
-}
-
-class MapScreen extends StatefulWidget {
-  @override
-  State<MapScreen> createState() => _MapScreenState();
-}
-
-class _MapScreenState extends State<MapScreen> {
-  LatLng latLng = const LatLng(33.9955811, 51.4268625);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          String url =
-              'https://www.google.com/maps/search/?api=1&query=${latLng.latitude},${latLng.longitude}';
-          if (await canLaunch(url)) {
-            await launch(url);
-          } else {
-            throw 'Could not launch $url';
-          }
-        },
-        child: const Icon(Icons.location_on),
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: FlutterMap(
-              options: MapOptions(
-                initialCenter: const LatLng(33.9955811, 51.4268625),
-                initialZoom: 15.0,
-                onTap: (tapPosition, point) {
-                  latLng = point;
-                  setState(() {});
-                },
-              ),
-              children: [
-                TileLayer(
-                  urlTemplate:
-                      'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-                  subdomains: const ['a', 'b', 'c'],
-                ),
-                MarkerLayer(
-                  markers: [
-                    Marker(
-                      width: 80.0,
-                      height: 80.0,
-                      point: LatLng(latLng.latitude, latLng.longitude),
-                      child: Container(
-                        child: Icon(
-                          Icons.location_on,
-                          color: Colors.red,
-                          size: 50.0,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.symmetric(vertical: 12),
-            child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  primary: const Color(0xff3D5920),
-                  minimumSize: const Size(200, 55),
-                ),
-                onPressed: () {},
-                child: const Text(
-                  'افزودن مسیر',
-                  style: TextStyle(
-                    fontSize: 22,
-                    fontWeight: FontWeight.w800,
-                  ),
-                )),
-          ),
-        ],
-      ),
     );
   }
 }
