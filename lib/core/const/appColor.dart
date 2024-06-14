6146f162207e2853f28eb657acd7e0cb27afef7b
MaxBased <hoseintahan84@gmail.com>
2024-06-14 23:21:59 +0330

    add      appColor.dart

---
+++ lib/core/const/appColor.dart
@@ -0,0 +1,20 @@
+import 'dart:ui';
+
+class AppColor {
+  // static const primaryGreen=Color(0xff3D5920);
+  static const primaryPistachio = Color(0xffCDD971);
+  static const lightPink = Color(0xffFFEEDF);
+  static const lightWhite = Color(0xffffffff);
+  static const darkOrange = Color(0xffC97123);
+  static const primaryGreen = Color(0xff009959);
+  static const cardGrey = Color(0xffF3F5F7);
+  static const priceRed = Color(0xffFF324B);
+  static const headerButtonStroke = Color(0xffF1F1F5);
+  static const headerButton = Color(0xffffffff);
+  static const productDescription = Color(0xff979899);
+  static const featureCard = Color(0xffF1F1F5);
+  static const featureCardText = Color(0xff979899);
+  static const addToCart = Color(0xff23AA49);
+
+
+}
