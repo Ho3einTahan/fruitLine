e6832eddec4e43caa40a6451e5c69f39c03ca951
MaxBased <hoseintahan84@gmail.com>
2024-06-14 23:18:52 +0330

    Update       main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:fruitline/core/class/model/cart.dart';
 import 'package:fruitline/core/class/model/map.dart';
+import 'package:fruitline/core/const/appColor.dart';
+import 'package:fruitline/view/screen/detail-product-screen.dart';
 import 'package:fruitline/view/screen/map-screen.dart';
+import 'package:fruitline/view/screen/splash-screen.dart';
+import 'package:fruitline/view/screen/vegetables-screen.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/adapters.dart';
 
@@ -22,8 +26,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        useMaterial3: true,
+        inputDecorationTheme: InputDecorationTheme(
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8),
+            borderSide: BorderSide.none,
+          ),
+          fillColor: AppColor.primaryPistachio.withOpacity(0.16),
+          filled: true,
+        ),
+      ),
       debugShowCheckedModeBanner: false,
-      home: MapScreen(),
+      home: ProductDetail_Screen(),
     );
   }
 }
