ef6e7e7416d316cae45f514147fa1924cd5496ce
MaxBased <hoseintahan84@gmail.com>
2024-06-14 23:17:06 +0330

    Update      splash-screen.dart

--- lib/view/screen/splash-screen.dart
+++ lib/view/screen/splash-screen.dart
@@ -1,11 +1,9 @@
 import 'dart:async';
 
-import 'package:animated_size_and_fade/animated_size_and_fade.dart';
 import 'package:flutter/material.dart';
 import 'package:fruitline/view/screen/hom-screen.dart';
 
 import '../../core/const/appColor.dart';
-import '../widget/fade-page-route.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -15,27 +13,16 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-  int index = 0;
-
   @override
   void initState() {
-    Timer.periodic(
-      const Duration(seconds: 2), // تغییر اینجا
-      (timer) {
-        setState(() {
-          if (index < 1 && index >= 0) {
-            index++;
-          } else {
-            index = 0;
-            timer.cancel();
-            Navigator.of(context).pushAndRemoveUntil(
-              FadePageRoute(page: const HomeScreen()),
-              (route) => false,
-            );
-          }
-        });
-      },
-    );
+    Timer.periodic(const Duration(seconds: 3), // تغییر اینجا
+        (timer) {
+      timer.cancel();
+      Navigator.of(context).pushAndRemoveUntil(
+        MaterialPageRoute(builder: (context) => const HomeScreen()),
+        (route) => false,
+      );
+    });
 
     super.initState();
   }
@@ -43,65 +30,10 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: AnimatedSizeAndFade(
-          fadeDuration: const Duration(seconds: 1),
-          sizeDuration: const Duration(seconds: 1),
-          alignment: Alignment.center,
-          child: screens[index]),
+      backgroundColor: AppColor.primaryGreen,
+      body: Center(
+        child: Image.asset('images/logo.png'),
+      ),
     );
   }
-}
-
-List<Widget> get screens {
-  return [
-    firstScreen(),
-    secondScreen(),
-  ];
-}
-
-Widget firstScreen() {
-  return Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: [
-      Padding(
-        padding: const EdgeInsets.only(left: 32, top: 64, bottom: 20),
-        child: Image.asset('images/avecado-icon.png', width: 150, height: 150),
-      ),
-      const Center(
-        child: Text('فروش رسمی میوه به صورت آنلاین',
-            style: TextStyle(
-              fontSize: 22,
-              fontWeight: FontWeight.bold,
-              color: AppColor.primaryGreen,
-            )),
-      ),
-      const Spacer(),
-      Image.asset('images/fruit-basket.png', height: 300, width: 300),
-    ],
-  );
-}
-
-Widget secondScreen() {
-  return Container(
-    decoration: const BoxDecoration(
-      image: DecorationImage(
-          fit: BoxFit.cover, image: AssetImage('images/splash-background.png')),
-    ),
-    child: Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        const SizedBox(height: 42),
-        Image.asset('images/avecado-icon.png', height: 150, width: 150),
-        const SizedBox(height: 20),
-        const Center(
-          child: Text('پرداخت درب منزل',
-              style: TextStyle(
-                  color: AppColor.primaryGreen,
-                  fontSize: 22,
-                  fontWeight: FontWeight.bold)),
-        ),
-      ],
-    ),
-  );
 }
