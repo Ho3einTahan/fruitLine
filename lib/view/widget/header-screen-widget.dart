630aa0c8c1533d4d0fa458d54a85416e2886de43
MaxBased <hoseintahan84@gmail.com>
2024-06-15 21:39:14 +0330

    Update      header-screen-widget.dart

--- lib/view/widget/header-screen-widget.dart
+++ lib/view/widget/header-screen-widget.dart
@@ -1,29 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:fruitline/core/const/appColor.dart';
+import 'package:get/get.dart';
 
 Widget headerBuilder({required String title}) {
   return Padding(
-    padding: const EdgeInsets.symmetric(vertical: 32, horizontal: 24),
+    padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 24),
     child: Row(
       children: [
         _buildHeaderButton(),
-        const SizedBox(width: 110),
-        Text(title, style: const TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+        Expanded(
+          child: Center(
+            child: Text(
+              title,
+              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
       ],
     ),
   );
 }
 
 Widget _buildHeaderButton() {
-  return Container(
-    height: 55,
-    width: 55,
-    decoration: BoxDecoration(
-      color: AppColor.headerButton,
-      border: Border.all(color: AppColor.headerButtonStroke, width: 1),
-      borderRadius: BorderRadius.circular(32),
+  return GestureDetector(
+    onTap: () => Get.back(),
+    child: Container(
+      height: 55,
+      width: 55,
+      decoration: BoxDecoration(
+        color: AppColor.headerButton,
+        border: Border.all(color: AppColor.headerButtonStroke, width: 1),
+        borderRadius: BorderRadius.circular(32),
+      ),
+      child: const Center(child: FaIcon(Icons.arrow_back_ios_new_outlined)),
     ),
-    child: const Center(child: FaIcon(Icons.arrow_back_ios_new_outlined)),
   );
 }
